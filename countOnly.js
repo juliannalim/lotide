c406754ded9acf31b66ebccd34c379e9f5cfcc7b
Julianna <julianna.j.lim@gmail.com>
2020-07-22 15:10:35 -0400

    the function shoud report back how many instances of each string were found in listOfName array of string

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  }
+};
+
+
+const countOnly = function (listOfName, itemsToCount) {
+  const results = {};
+
+  for (const name of listOfName) {
+    // if itemsToCount[name] === true
+    //skip any itemsToCount[name] that is not true
+    // aka "jason" === true
+    if (itemsToCount[name]) {
+      if (results[name]) {
+        results[name] += 1;
+      } else {
+        results[name] = 1;
+      }
+    }
+    // console.log(name);
+  }
+
+  return results;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
